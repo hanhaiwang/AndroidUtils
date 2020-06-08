bc6b723c537eab5daa44064ccbb193c143ef4599
hanhaiwang <16702426@qq.com>
2020-06-08 15:38:42 +0800

    AndroidUtils

---
+++ app/src/main/java/com/hanhaiwang/androidutils/adapter/CommonRecyclerAdapter.java
@@ -0,0 +1,56 @@
+package com.hanhaiwang.androidutils.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+/**
+ * Description: 通用的Adapter
+ */
+public abstract class CommonRecyclerAdapter<T> extends RecyclerView.Adapter<ViewHolder> {
+
+    protected Context mContext;
+    protected LayoutInflater mInflater;
+    //数据怎么办？利用泛型
+    protected List<T> mDatas;
+    // 布局怎么办？直接从构造里面传递
+    private int mLayoutId;
+
+    public CommonRecyclerAdapter(Context context, List<T> datas, int layoutId) {
+        this.mContext = context;
+        this.mInflater = LayoutInflater.from(mContext);
+        this.mDatas = datas;
+        this.mLayoutId = layoutId;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // 先inflate数据
+        View itemView = mInflater.inflate(mLayoutId, parent, false);
+        // 返回ViewHolder
+        ViewHolder holder = new ViewHolder(itemView);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // 绑定怎么办？回传出去
+        convert(holder, mDatas.get(position));
+    }
+
+    /**
+     * 利用抽象方法回传出去，每个不一样的Adapter去设置
+     * @param item 当前的数据
+     */
+    public abstract void convert(ViewHolder holder, T item);
+
+    @Override
+    public int getItemCount() {
+        return mDatas.size();
+    }
+}
---
+++ app/src/main/java/com/hanhaiwang/androidutils/view/RoundImageView.java
@@ -0,0 +1,105 @@
+package com.hanhaiwang.androidutils.view;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+/**
+ * 自宝义ImageView布中的圆形图片
+ * Created by: hanhaiwang.com
+ * Data: 2019-08-07
+ * Time: 11:57
+ * Author: Bin
+ **/
+public class RoundImageView extends AppCompatImageView {
+
+    public RoundImageView(Context context) {
+        super(context);
+    }
+
+    public RoundImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public RoundImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    //自定义View实现过程中很重要的onDraw绘制图形的方法
+    @Override
+    protected void onDraw(Canvas canvas) {
+
+        Drawable drawable = getDrawable();
+
+        //空值判断，必要步骤，避免由于没有设置src导致的异常错误
+        if (drawable == null) {
+            return;
+        }
+
+        //必要步骤，避免由于初始化之前导致的异常错误
+        if (getWidth() == 0 || getHeight() == 0) {
+            return;
+        }
+
+        if (!(drawable instanceof BitmapDrawable)) {
+            return;
+        }
+        Bitmap b = ((BitmapDrawable) drawable).getBitmap();
+
+        if (null == b) {
+            return;
+        }
+
+        Bitmap bitmap = b.copy(Bitmap.Config.ARGB_8888, true);
+
+        int w = getWidth();
+
+        Bitmap roundBitmap = getCroppedBitmap(bitmap, w);
+        canvas.drawBitmap(roundBitmap, 0, 0, null);
+
+    }
+
+    /**
+     * 初始Bitmap对象的缩放裁剪过程
+     * @param bmp       初始Bitmap对象
+     * @param radius    圆形图片直径大小
+     * @return 返回一个圆形的缩放裁剪过后的Bitmap对象
+     */
+    public static Bitmap getCroppedBitmap(Bitmap bmp, int radius) {
+        Bitmap sbmp;
+        //比较初始Bitmap宽高和给定的圆形直径，判断是否需要缩放裁剪Bitmap对象
+        if (bmp.getWidth() != radius || bmp.getHeight() != radius)
+            sbmp = Bitmap.createScaledBitmap(bmp, radius, radius, false);
+        else
+            sbmp = bmp;
+        Bitmap output = Bitmap.createBitmap(sbmp.getWidth(), sbmp.getHeight(),
+                Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, sbmp.getWidth(), sbmp.getHeight());
+
+        paint.setAntiAlias(true);
+        paint.setFilterBitmap(true);
+        paint.setDither(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(Color.parseColor("#BAB399"));
+        canvas.drawCircle((float) sbmp.getWidth() / 2 , (float) sbmp.getHeight() / 2 , (float) sbmp.getWidth() / 2 , paint);
+        //核心部分，设置两张图片的相交模式，在这里就是上面绘制的Circle和下面绘制的Bitmap
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(sbmp, rect, rect, paint);
+
+        return output;
+    }
+}
